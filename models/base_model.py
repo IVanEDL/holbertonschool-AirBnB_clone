c7efb42ea378a9c3a813b93a00f0af0fd9fd4927
EliasMartincorre <root@7e3f4dfdda52.(none)>
2023-02-23 08:48:45 -0800

    isoformat()

--- models/base_model.py
+++ models/base_model.py
@@ -4,12 +4,12 @@ import datetime
 class BaseModel:
     def __init__(self, id=None):
         self.id = str(uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
     
     def save(self):
         """update"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now().isoformat()
 
     def __str__(self):
         return f"[{__class__.__name__}] ({self.id}) ({self.__dict__})"
