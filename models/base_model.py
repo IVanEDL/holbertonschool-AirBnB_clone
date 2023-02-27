006d16427f28dcf667515fd4fbb44a4ed3b33cc1
EliasMartincorre <root@e89f06ff8bf5.git>
2023-02-27 06:50:38 -0800

    pycodestyle and unittesst for basemodel method

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,12 @@
 from uuid import uuid4
 import datetime
 from models import storage
+
+
 class BaseModel:
+    """This class is the model basis on which the web application
+       will be based."""
+
     def __init__(self, *args, **kwargs):
         if len(kwargs) != 0:
             for key, value in kwargs.items():
@@ -10,7 +15,8 @@ class BaseModel:
                     pass
                 else:
                     try:
-                        setattr(self, key, datetime.datetime.fromisoformat(value))
+                        a = datetime.datetime.fromisoformat(value)
+                        setattr(self, key, a)
                     except Exception:
                         setattr(self, key, value)
         else:
@@ -18,7 +24,7 @@ class BaseModel:
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
             storage.new(self)
-    
+
     def save(self):
         """update"""
         self.updated_at = datetime.datetime.now()
@@ -26,19 +32,20 @@ class BaseModel:
 
     def __str__(self):
         return f"[{__class__.__name__}] ({self.id}) ({self.__dict__})"
-    
+
     def to_dict(self):
         r = {}
-        r ["__class__"] = __class__.__name__
+        r["__class__"] = __class__.__name__
         for key, value in self.__dict__.items():
-                r[key] = value
+            r[key] = value
         r["created_at"] = self.created_at.isoformat()
         r["updated_at"] = self.updated_at.isoformat()
         return r
-   
-   
+
     def my_number(self, number):
+        """ return a number"""
         return number
-   
-    def name(self,name):
+
+    def name(self, name):
+        """ return name for the person"""
         return name
