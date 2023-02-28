94d81f89dcebee585d67ff46a9559b1642c52f65
EliasMartincorre <root@e89f06ff8bf5.git>
2023-02-28 06:03:21 -0800

    fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     Class Filestorage:
-    this class provide ths few methods 
+    this class provide ths few methods
     for work whit object, and json files
     return a dictionary from
     instance and file.json
@@ -9,36 +9,51 @@
     """
 import json
 import os
+
+
 class FileStorage:
     """ Class """
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        return self.__objects
-    
-    def new(self, obj):
-        self.__objects[f'{obj.__class__.__name__}.{getattr(obj, "id")}'] = obj
+        return FileStorage.__objects
 
-    
+    def new(self, obj):
+        a = f'{obj.__class__.__name__}.{getattr(obj, "id")}'
+        FileStorage.__objects[a] = obj
+
     def save(self):
         dic = {}
-        if len(self.__objects) > 0:
+        if len(FileStorage.__objects) > 0:
             for key, value in self.__objects.items():
                 try:
                     dic[key] = value.to_dict()
                 except Exception:
                     dic[key] = value
-            with open(self.__file_path, "w") as f:
+            with open(FileStorage.__file_path, "w") as f:
                 json.dump(dic, f)
         else:
             pass
 
     def reload(self):
         """ return list of instan*ce from json file"""
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path) as h:
-                self.__objects = json.load(h)
+
+        from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
+
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path) as h:
+                dictionary = json.load(h)
+                for key, value in dictionary.items():
+                    obj_class = value['__class__']
+                    obj_instance = eval(obj_class + "(**value)")
+                    FileStorage.__objects[key] = obj_instance
         else:
             pass
