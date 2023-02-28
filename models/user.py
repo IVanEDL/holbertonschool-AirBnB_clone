a8e2cf7e600bdae4a990fcc41866627cf0ef7399
EliasMartincorre <root@e89f06ff8bf5.git>
2023-02-28 08:14:29 -0800

    hotfixed

--- models/user.py
+++ models/user.py
@@ -9,5 +9,5 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-    def __init__(self):
-        self.id = BaseModel().id
+    def __init__(self, *args, **kwargs):
+        super().__init__()
